5bc781792c5d2cac42f05f505db1d6f215dde217
Artur <aginter11@ya.ru>
2020-11-20 10:57:15 +0500

    1st commit from local repo

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello! How are you?");
-        System.out.println("From remote repository");
+        System.out.println("From local repository");
+        System.out.println("Added line in local repo");
     }
 }
