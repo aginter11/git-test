c03b0b9b61265f30f50ca16398982e149264b4d1
Артур Галиев <66908573+aginter11@users.noreply.github.com>
2020-11-20 11:05:02 +0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello! How are you?");
         System.out.println("From local repository");
         System.out.println("Added line in local repo");
+         System.out.println("Added line2 in remote repo");
     }
 }
